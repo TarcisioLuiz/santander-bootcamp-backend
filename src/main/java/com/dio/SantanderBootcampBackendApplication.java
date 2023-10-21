80555e69c88e0ebb56ffbf903dcb8c187f1e4791
TarcisioLuiz <tarcisioluizfilho@gmail.com>
2023-10-21 00:11:08 -0300

    Initial commit

---
+++ src/main/java/com/dio/SantanderBootcampBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcampBackendApplication.class, args);
+	}
+
+}
